9021919fa8dd6edcd417cc1643a8e01e9453025a
Scott Davenport <scott@thinkcode.ai>
2025-02-25 18:39:47 -0500

    Integrate Cloudinary and Supabase for sponsor carousel

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-import { Database } from './database.types';
+import { Database } from './types';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
